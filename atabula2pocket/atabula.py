a7b8c74697abfb8bc45da7746a09cb7b91b4bb05
Julien Danjou <julien@danjou.info>
2019-09-07 15:57:07 +0200

    Find nonce dynamically

--- atabula2pocket/atabula.py
+++ atabula2pocket/atabula.py
@@ -1,17 +1,25 @@
 import os
+import re
 
 import requests
 
+NONCE = re.compile(
+    "name=\"woocommerce-login-nonce\"\s+value=\"([^\"]+)\""
+)
 
 def get_session():
+    login_url = "https://www.atabula.com/mon-compte/"
     session = requests.Session()
+    login_page = session.get(login_url)
+    nonce = NONCE.search(login_page.text).group(1)
+
     ret = session.post(
         # Don't forget the www or you get a 301 to it
-        "https://www.atabula.com/mon-compte/",
+        login_url,
         data={
             "username": os.getenv("ATABULA_USERNAME"),
             "password": os.getenv("ATABULA_PASSWORD"),
-            "woocommerce-login-nonce": "29228d406d",
+            "woocommerce-login-nonce": nonce,
             "_wp_http_referer": "/mon-compte%/",
             "login": "Connection",
         },
